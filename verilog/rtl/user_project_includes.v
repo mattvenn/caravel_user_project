406107547b33391af94a2141997e9a26100491d7
Matt Venn <matt@mattvenn.net>
2021-05-31 15:31:21 +0200

    includes

---
+++ verilog/rtl/user_project_includes.v
@@ -0,0 +1,5 @@
+//  1 ('Frequency counter',)         : ../course/wrapped_frequency_counter
+`include "wrapped_frequency_counter/wrapper.v"
+`include "wrapped_frequency_counter/frequency_counter/src/edge_detect.v"
+`include "wrapped_frequency_counter/frequency_counter/src/frequency_counter.v"
+`include "wrapped_frequency_counter/frequency_counter/src/seven_segment.v"
