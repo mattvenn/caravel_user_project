2c840bd5e1c15e326a69b6502c542f2670c1c63a
Matt Venn <matt@mattvenn.net>
2021-06-17 17:46:55 +0200

    includes

---
+++ verilog/rtl/user_project_includes.v
@@ -0,0 +1,32 @@
+//  0 ('RGB Mixer',)                 : /home/matt/work/asic-workshop/course/wrapped_rgb_mixer
+`include "wrapped_rgb_mixer/wrapper.v"
+//  1 ('Frequency counter',)         : /home/matt/work/asic-workshop/course/wrapped_frequency_counter
+`include "wrapped_frequency_counter/wrapper.v"
+//  2 ('A5/1 Wishbone',)             : /home/matt/work/asic-workshop/course/participants/group1/jamieiles/a5-1-wb-macro
+`include "a5-1-wb-macro/src/wrapper.v"
+//  3 ('Fibonacci',)                 : /home/matt/work/asic-workshop/course/participants/group3/konrad/fibonacci
+`include "fibonacci/src/wrapper.v"
+//  5 ('Quad PWM FET Drivers',)      : /home/matt/work/asic-workshop/course/participants/group2/chris/wrapped_quad_pwm_fet_drivers
+`include "wrapped_quad_pwm_fet_drivers/wrapper.v"
+//  7 ('memLCDdriver',)              : /home/matt/work/asic-workshop/course/participants/group3/matthewb/wrapped_memLCDdriver
+`include "wrapped_memLCDdriver/wrapper.v"
+//  8 ('QARMA-64 Accelerator',)      : /home/matt/work/asic-workshop/course/participants/group4/brange/wrapped_qarma
+`include "wrapped_qarma/wrapper.v"
+//  9 ('ChaCha20 Accelerator',)      : /home/matt/work/asic-workshop/course/participants/group3/petri/wrapped_chacha_wb_accel
+`include "wrapped_chacha_wb_accel/wrapper.v"
+// 10 ('Framebufferless Video Core',) : /home/matt/work/asic-workshop/course/participants/group2/tomg/wrapped-fbless-graphics-core
+`include "wrapped-fbless-graphics-core/wrapper.v"
+//  4 ('Pong',)                      : /home/matt/work/asic-workshop/course/participants/group2/ericz/wrapped_pong
+`include "wrapped_pong/wrapper.v"
+//  6 ('Hack soc',)                  : /home/matt/work/asic-workshop/course/participants/group3/mbalestrini/wrapped_hack_soc
+`include "wrapped_hack_soc/wrapped_hack_soc.v"
+// 12 ('gfxdemo',)                   : /home/matt/work/asic-workshop/course/participants/kbeckmann/wrapped_gfxdemo
+`include "wrapped_gfxdemo/wrapper.v"
+// 11 ('Wishbone HyperRAM',)         : /home/matt/work/asic-workshop/course/participants/group2/pawel/wrapped_wb_hyperram
+`include "wrapped_wb_hyperram/wrapper.v"
+// 15 ('Newmot SoC',)                : /home/matt/work/asic-workshop/course/participants/openlane/wrapped_newmot
+`include "wrapped_newmot/wrapper.v"
+// 13 ('hoggephase',)                : /home/matt/work/asic-workshop/course/participants/openlane/wrapped_hoggephase_project
+`include "wrapped_hoggephase_project/wrapper.v"
+// 14 ('bfloat16_fma',)              : /home/matt/work/asic-workshop/course/participants/openlane/bfloat16_fma
+`include "bfloat16_fma/wrapper.v"
