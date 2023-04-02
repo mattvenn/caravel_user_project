ff23b71ff84fd8e09e3c2b79c7b2fcb9301fcec5
M0stafaRady <mostafa.rady@efabless.com>
2023-04-02 11:10:04 -0700

    update regressions list

--- verilog/dv/cocotb/cocotb_includes.py
+++ verilog/dv/cocotb/cocotb_includes.py
@@ -1,6 +1,6 @@
 from os import path
 import sys
-sys.path.append(path.abspath('/home/rady/caravel/swift/caravel-dynamic-sims/cocotb'))
+sys.path.append(path.abspath('/home/rady/caravel/caravel_release/caravel-dynamic-sims/cocotb'))
 from tests.common_functions.test_functions import repot_test
 from tests.common_functions.test_functions import test_configure
 from tests.common_functions.test_functions import max_num_error
